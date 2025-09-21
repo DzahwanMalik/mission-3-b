72f3aae25b55e55bcf39e5f62f6ea62325e918c2
DzahwanMalik <malikdzahwan22@gmail.com>
2025-09-22 04:37:48 +0700

    fix: bug dropwdown navbar

--- modules/Dropdown.js
+++ modules/Dropdown.js
@@ -1,6 +1,7 @@
 function showDropdown() {
   const profile = document.getElementById("profile");
   const dropdown = document.getElementById("dropdown");
+  const links = document.querySelectorAll(`#dropdown li a`)
 
   let showed = false;
 
@@ -14,6 +15,10 @@ function showDropdown() {
       dropdown.classList.add("translate-y-0");
       dropdown.classList.add("opacity-100");
 
+      links.forEach(link => {
+        link.classList.replace('hidden', 'flex');
+      });
+
       showed = true;
     } else {
       dropdown.classList.remove("z-20");
@@ -23,6 +28,12 @@ function showDropdown() {
       dropdown.classList.add("-z-10");
       dropdown.classList.add("-translate-y-5");
       dropdown.classList.add("opacity-0");
+
+      links.forEach(link => {
+        setTimeout(() => {
+          link.classList.replace('flex', 'hidden');
+        }, 300)
+      });
 
       showed = false;
     }
